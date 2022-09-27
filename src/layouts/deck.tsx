2c82e5fbfb967d29f37c24f7d54be709b895f127
David Yell <david.yell@mdgroup.com>
2022-09-27 09:26:28 +0100

    Started trying to create some basic bits

---
+++ src/layouts/deck.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Deck = () => {
+  return (
+    <p>I am the deck view, edit and build layout!</p>
+  )
+}
+
+export default Deck;
---
+++ src/layouts/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <p>Hello, I am the home page!</p>
+  )
+}
+
+export default Home;
