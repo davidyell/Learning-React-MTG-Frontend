45f3b28e8826be10c562913a2cf9ec631ed74144
David Yell <david.yell@mdgroup.com>
2022-09-28 15:44:12 +0100

    Temporarily FE fixed the recent list to 5 until the api is updated
    See https://github.com/davidyell/Learning-Express-MTG-Api/issues/48

--- src/decks/latestDecks.tsx
+++ src/decks/latestDecks.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Typography } from 'antd'
+import { Button, Typography } from 'antd'
 import Axios from 'axios'
 import type { DeckList, DeckListItem } from '../types/decks.type'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import DeckSummary from './deckSummary'
+import { CaretRightOutlined } from '@ant-design/icons'
 
 dayjs.extend(relativeTime);
 const { Title } = Typography;
@@ -17,6 +18,8 @@ const LatestDecks = () => {
 
     Axios.get<DeckList>(url)
       .then((response) => {
+        const decksData = response.data.data;
+        decksData.sort((a, b) => a.deck.updated > b.deck.updated ? -1 : 1);
         setDecks(response.data.data)
       });
   }
@@ -25,7 +28,7 @@ const LatestDecks = () => {
     getDecks();
   }, []);
 
-  const recentDecks = decks.map((deckItem: DeckListItem) => {
+  const recentDecks = decks.slice(0, 5).map((deckItem: DeckListItem) => {
     return <li key={deckItem.deck.id} style={{ listStyle: 'none', marginBottom: '8px' }}>
       <DeckSummary deckItem={deckItem} />
     </li>
@@ -35,6 +38,7 @@ const LatestDecks = () => {
     <>
       <Title level={2}>Latest decks</Title>
       <ul>{recentDecks}</ul>
+      <Button type='primary' title="See more decks" style={{ float: 'right' }} href='/decks'>View all decks <CaretRightOutlined /></Button>
     </>
   )
 }
